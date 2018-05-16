042273160f448408dffea74ccfd0a8721e708b5e
Elliot Derhay <e.j.derhay@gmail.com>
2018-05-16 09:12:06 -0400

    Try out my own color constants
    These constants represent the colors I've decided on currently. They very
    well could change before release 1.0, especially since the colors of the
    Now webpage have been greatly simplified since I was last on there.

--- index.js
+++ index.js
@@ -3,31 +3,40 @@ const backgroundColor = '#000000';
 const foregroundColor = '#cbcfd4';
 
 // Colors
-
+const L_RED = '#ff5b53';
+const MAGENTA = '#ff0080';
+const L_MAGENTA = '#ff69b4';
+const D_YELLOW = '#f1ae13';
+const L_GREEN = '#61e078';
+const L_CYAN = '#77c8f2';
+const L_PURPLE = '#ae81ff';
+const WHITE = '#ffffff';
+const L_GRAY = '#ababab';
+const D_GRAY = '#3f3f3f';
 
 // Mapped Colors
 const colors = {
   black: backgroundColor,
-  red: '#ff69b4',
-  green: '#61e078',
-  yellow: '#f1ae13',
-  blue: '#ae81ff',
-  magenta: '#3f3f3f',
-  cyan: '#ababab',
-  white: '#ffffff',
-  lightBlack: '#3f3f3f',
-  lightRed: '#ff5b53',
-  lightGreen: '#61e078',
-  lightYellow: '#f1ae13',
-  lightBlue: '#ff5b53',
-  lightMagenta: '#ff69b4',
-  lightCyan: '#77c8f2',
-  colorCubes: '#ababab',
+  red: MAGENTA,
+  green: L_GREEN,
+  yellow: D_YELLOW,
+  blue: L_PURPLE,
+  magenta: D_GRAY,
+  cyan: L_GRAY,
+  white: WHITE,
+  lightBlack: D_GRAY,
+  lightRed: L_MAGENTA,
+  lightGreen: L_GREEN,
+  lightYellow: D_YELLOW,
+  lightBlue: L_RED,
+  lightMagenta: L_MAGENTA,
+  lightCyan: L_CYAN,
+  colorCubes: L_GRAY,
   grayscale: foregroundColor
 };
 
 // Additional Constants
-const cursorColor = '#77c8f2';
+const CURSOR_COLOR = L_CYAN;
 const borderColor = backgroundColor;
 
 exports.decorateConfig = (config) => {
@@ -40,8 +49,8 @@ exports.decorateConfig = (config) => {
     css: `
       ${config.css || ''}
       .cursor-node {
-        background-color: ${'#ff69b4'} !important;
-        border-color: ${'#ff69b4'} !important;
+        background-color: ${L_MAGENTA} !important;
+        border-color: ${L_MAGENTA} !important;
       }
       .hyper_main {
         border: none !important;
@@ -56,7 +65,7 @@ exports.decorateConfig = (config) => {
         border: 0;
       }
       .tab_textActive {
-        border-bottom: 2px solid ${'#f1ae13'};
+        border-bottom: 2px solid ${D_YELLOW};
       }
     `
   })
